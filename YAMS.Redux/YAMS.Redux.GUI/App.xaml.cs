310aa1ba887a2e9e38d62e2fbb16752c0164a0ee
Mikael Norée <Mikael.Noree@apoteket.se>
2020-02-14 15:52:36 +0100

    Adding projects.

--- YAMS.Redux/YAMS.Redux.GUI/App.xaml.cs
+++ YAMS.Redux/YAMS.Redux.GUI/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace YAMS.Redux
+namespace YAMS.Redux.GUI
 {
     /// <summary>
     /// Interaction logic for App.xaml
