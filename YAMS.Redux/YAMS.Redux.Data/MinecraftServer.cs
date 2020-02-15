9fbffb3ca3550c744d0be5a772d47c20d288fd2a
Mikael Norée <mikael.noree@outlook.com>
2020-02-15 09:30:23 +0100

    Adding start of datamodel.

---
+++ YAMS.Redux/YAMS.Redux.Data/MinecraftServer.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YAMS.Redux.Data
+{
+    public class MinecraftServer
+    {
+        [Key]
+        public int ServerId { get; set; }
+
+        [StringLength(255)]
+        public string ServerName { get; set; }
+
+        public bool ServerEnableServerOptimisations { get; set; }
+
+        public int ServerAssignedMemory { get; set; }
+
+        public bool ServerAutoStart { get; set; }
+
+        [StringLength(100)]
+        public string ServerType { get; set; }
+
+    }
+
+}
---
+++ YAMS.Redux/YAMS.Redux.Data/YAMSDatabase.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace YAMS.Redux.Data
+{
+    public class YAMSDatabase : DbContext
+    {
+
+        protected static string CONNECTIONNAME;
+
+        public YAMSDatabase(string ConnectionName = "YAMS")
+        {
+            CONNECTIONNAME = ConnectionName;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings[CONNECTIONNAME].ConnectionString,
+            providerOptions => providerOptions.EnableRetryOnFailure());
+        }
+
+        public virtual DbSet<MinecraftServer> Servers { get; set; }
+
+    }
+
+}
