61ef15faf63faa3ecd9043779c772f867a2730d1
Mikael Norée <mikael.noree@outlook.com>
2020-02-16 22:23:37 +0100

    Adding to the datamodel.

---
+++ YAMS.Redux/YAMS.Redux.Core/AppCore.cs
@@ -0,0 +1,63 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+
+namespace YAMS.Redux.Core
+{
+    public static class AppCore
+    {
+
+        private static Logger MyLog { get; set; }
+
+        public static bool IsService { get; private set; }
+        public static string YAMSVersion
+        {
+            get
+            {
+                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+                // string assemblyVersion = Assembly.LoadFile('your assembly file').GetName().Version.ToString();
+                // string fileVersion = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion;
+                // string productVersion = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
+            }
+        }
+
+        /// <summary>
+        /// Main starting point for app/service.
+        /// </summary>
+        public static void Execute()
+        {
+            if (MyLog == null) MyLog = LogManager.GetCurrentClassLogger();
+            MyLog.Info("*** Execute of {YAMSVersion} ***", YAMSVersion);
+
+            // Check if we are a service
+            Process cp = Process.GetCurrentProcess();
+            if (cp.SessionId > 0)
+                IsService = false;
+            else
+                IsService = true;
+            MyLog.Debug("Running as service => " + IsService);
+            // Done
+
+            // Open connection to database.
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                MyLog.Error(ex, "Datasource connection failed.");
+                throw; // We need to end here, no point with out data?
+            }
+
+
+
+            MyLog.Info("Start completed, YAMS.Redux is now running.");
+
+        }
+
+    }
+
+}
---
+++ YAMS.Redux/YAMS.Redux.Data/Mojang/MojangManifest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YAMS.Redux.Data.Mojang
+{
+    public class MojangManifest
+    {
+        public latest Latest { get; set; }
+        public List<MojangVersion> versions { get; set; }
+
+        /// <summary>
+        /// Get the latest release from mojang
+        /// </summary>
+        /// <returns></returns>
+        public MojangVersion GetLatestRelease()
+        {
+            if (Latest == null) return null;
+            return versions.Find(v => v.id == Latest.release);
+        }
+        /// <summary>
+        /// Get the latest weekly snaposhot
+        /// </summary>
+        /// <returns></returns>
+        public MojangVersion GetLatestSnapshot()
+        {
+            if (Latest == null) return null;
+            return versions.Find(v => v.id == Latest.snapshot);
+        }
+
+    }
+
+
+    public class MojangVersion
+    {
+        public string id { get; set; }
+        public string type { get; set; }
+        public DateTime time { get; set; }
+        public DateTime releaseTime { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class latest
+    {
+        public string snapshot { get; set; }
+        public string release { get; set; }
+    }
+
+    public class MojangDownloadFile
+    {
+        public MojangAsset assetIndex { get; set; }
+        public string assets { get; set; }
+        public MojangDownloads downloads { get; set; }
+        public string id { get; set; }
+
+    }
+
+    public class MojangDownloads
+    {
+        public string id { get; set; }
+        public MojangDownloadsItem client { get; set; }
+        public MojangDownloadsItem server { get; set; }
+
+    }
+
+    public class MojangDownloadsItem
+    {
+        public string sha1 { get; set; }
+        public int size { get; set; }
+        public string url { get; set; }
+    }
+
+    public class MojangAsset
+    {
+        public string id { get; set; }
+        public string sh1 { get; set; }
+        public int size { get; set; }
+        public string url { get; set; }
+        public int totalSize { get; set; }
+
+    }
+
+}
